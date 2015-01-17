a9e0ffed09d9ab8223ad6c15209a482c6ca9649d
Lucas <godkiller_nt@hammermaps.de>
2015-01-17 21:27:31 +0100

    Update Config

--- inc/_templates_/version1.6/_js/template.js
+++ inc/_templates_/version1.6/_js/template.js
@@ -1,13 +1,40 @@
 /*
  * CKEditor - WYSIWYG Options
  */
+var config_ckeditor_standard = {
+	toolbar:
+	[
+		['Cut','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+		['Undo','Redo','-','SelectAll','RemoveFormat'],
+		['Bold','Italic','Underline','Strike'],
+		['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Link','Unlink','Image','HorizontalRule','Anchor'],
+		['Styles','Format','Font','FontSize'],
+		['TextColor','BGColor'],
+		['Maximize', '-','Source']
+	],
+	
+	//{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	
+	language: 'eng',
+	coreStyles_bold: { element : 'b', overrides : 'strong' }
+};
+
 var config_ckeditor_mini = {
 	toolbar:
 	[
-		['Source','-','Save','NewPage','Preview','-','Templates'],
-		['Maximize', 'ShowBlocks','-','About']
+		['Cut','Paste','-', 'SpellChecker', 'Scayt'],
+		['Undo','Redo'],
+		['Bold','Italic','Underline','Strike'],
+		['NumberedList','BulletedList'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Link','Unlink','Image']
 	],
-		
+	
+	//{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	
+	language: 'eng',
 	coreStyles_bold: { element : 'b', overrides : 'strong' }
 };
 
@@ -28,6 +55,6 @@ var TPL = {
 	
 	//CKEditor - WYSIWYG 
 	CKEditor: function() {
-		$(".editorStyleWord").ckeditor(config_ckeditor_mini);
+		$(".editorStyleWord").ckeditor(config_ckeditor_standard);
 	}
 };
