85355db2b2c4bca3d3878517d0a7c300ede6a836
Lucas <godkiller_nt@hammermaps.de>
2015-04-25 10:34:00 +0200

    Delete V1.5 TPL & Update Glossar

--- glossar/index.php
+++ glossar/index.php
@@ -13,42 +13,57 @@ include(basePath."/inc/common.php");
 ## SETTINGS ##
 $dir = "glossar";
 $where = _glossar;
-$use_glossar = false;
+$use_glossar = false; //Disable Glossar Tags in Glossar
 
 ## SECTIONS ##
-$a = ''; $glword = '';
+$a = '';
 if(!empty($_GET['word'])) {
+    $_GET['word'] = trim($_GET['word']);
     $a = substr($_GET['word'],0,1);
-    $glword = "WHERE word = '".up($_GET['word'])."' OR word LIKE '".up($a)."%' ";
+    $qry = $sql->select("SELECT * "
+            . "FROM `{prefix_glossar}` "
+            . "WHERE `word` = ? OR `word` LIKE ? "
+            . "ORDER BY `word`;",
+            array(up($_GET['word']),'%'.up($a).'%'));
 } else if(!empty($_GET['bst']) && $_GET['bst'] != 'all') {
-    $glword = "WHERE word LIKE '".up($_GET['bst'])."%' ";
+    $_GET['bst'] = trim($_GET['bst']);
     $a = $_GET['bst'];
+    $qry = $sql->select("SELECT * "
+            . "FROM `{prefix_glossar}` "
+            . "WHERE `word` LIKE ? "
+            . "ORDER BY `word`;",
+            array('%'.up($a).'%'));
+} else {
+    $qry = $sql->select("SELECT * "
+            . "FROM `{prefix_glossar}` "
+            . "ORDER BY `word`;");
 }
 
-$qry = db("SELECT * FROM ".$db['glossar']." ".$glword." ORDER BY word");
-while($get = _fetch($qry)) {
+foreach($qry as $get) {
     $class = ($color % 2) ? "contentMainSecond" : "contentMainFirst"; $color++;
-
-    if(isset($_GET['word']) && $_GET['word'] == $get['word'])
+    if(isset($_GET['word']) && $_GET['word'] == $get['word']) {
         $class = 'highlightSearchTarget';
+    }
 
     $show .= show($dir."/glossar_show", array("word" => re($get['word']),
                                               "class" => $class,
                                               "glossar" => bbcode(re($get['glossar']))));
 }
 
-$bst = array(_all,"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"); $abc = '';
+$show = (empty($show) ? show(_no_entrys_found, array("colspan" => "2")) : $show); //No Entrys
+
+$bst = array(_all,"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"); $abc = ''; $i=0;
 foreach ($bst as $bst_s) {
-    $bclass = (empty($a) && ($bst_s) == _all || strtolower($bst_s) == strtolower($a)) ? 'fontWichtig' : '';
-    $ret = ($bst_s == _all) ? '?bst=all' : "?bst=".$bst_s;
-    $abc .= "<a href=\"".$ret."\" title=\"".$bst_s."\"><span class=\"".$bclass."\">".$bst_s."</span></a> ";
+    $bclass = (empty($a) && ($bst_s) == _all || strtolower($bst_s) == strtolower($a)) ? 'active' : '';
+    $ret = ($bst_s == _all) ? '?bst=all' : "?bst=".$bst_s; $i++;
+    $abc .= "<a href=\"".$ret."\" title=\"".$bst_s."\"><span class=\"pagination ".$bclass."\">".$bst_s."</span></a> ";
+    if($i == 20) {
+        $abc .= '<p>';
+    }
 }
 
-$index = show($dir."/glossar", array("head" => _glossar_head,
-                                     "word" => _glossar_bez,
-                                     "bez" => _glossar_erkl,
-                                     "abc" => $abc,
-                                     "show" => $show));
+$index = show($dir."/glossar", array("show" => $show, "abc" => $abc));
+
 ## INDEX OUTPUT ##
 $title = $pagetitle." - ".$where;
 page($index, $title, $where);
