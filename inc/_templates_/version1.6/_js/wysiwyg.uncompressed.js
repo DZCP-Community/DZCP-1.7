b834399b397e82288bd4d6deaa277f11118156d2
Lucas <godkiller_nt@hammermaps.de>
2015-05-11 22:57:31 +0200

    Fix for blockquote

--- inc/_templates_/version1.6/_js/wysiwyg.uncompressed.js
+++ inc/_templates_/version1.6/_js/wysiwyg.uncompressed.js
@@ -1,4 +1,4 @@
-tinyMCE_GZ.init({ plugins : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,dzcp,media', themes : 'advanced', languages : (dzcp_config.lng == 'de' ? dzcp_config.lng : 'en'), disk_cache : true, debug : false });
+tinyMCE_GZ.init({ plugins : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,media', themes : 'advanced', languages : (dzcp_config.lng == 'de' ? dzcp_config.lng : 'en'), disk_cache : true, debug : false });
 
 //default wysiwyg editor
 tinyMCE.init({
@@ -8,7 +8,7 @@ tinyMCE.init({
     plugins                             : 'contextmenu,dzcp,inlinepopups,spellchecker,media',
     language                            : (dzcp_config.lng == 'de' ? dzcp_config.lng : 'en'),
     theme_advanced_buttons1: 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,image,youtube,forecolor,'
-    + 'backcolor,|,smileys,flags,dzcpuser,|,spellchecker',
+    + 'backcolor,blockquote,|,smileys,flags,dzcpuser,|,spellchecker',
     theme_advanced_toolbar_location     : 'top',
     theme_advanced_toolbar_align        : 'center',
     theme_advanced_statusbar_location   : 'bottom',
@@ -52,7 +52,7 @@ tinyMCE.init({
     theme                               : 'advanced',
     plugins                             : 'contextmenu,dzcp,media',
     language                            : (dzcp_config.lng == 'de' ? dzcp_config.lng : 'en'),
-    theme_advanced_buttons1             : 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,image,forecolor,backcolor',
+    theme_advanced_buttons1             : 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,|,image,forecolor,backcolor,blockquote',
     theme_advanced_buttons2             : '',
     theme_advanced_buttons3             : '',
     theme_advanced_toolbar_location     : 'top',
@@ -78,7 +78,7 @@ tinyMCE.init({
     plugins                           : 'contextmenu,dzcp,advimage,paste,table,fullscreen,inlinepopups,spellchecker,searchreplace,insertdatetime,media',
     language                          : (dzcp_config.lng == 'de' ? dzcp_config.lng : 'en'),
     theme_advanced_buttons1           : 'bold,italic,underline,|,justifyleft,justifycenter,justifyright,justifyfull,|,undo,redo,|,bullist,numlist,|,link,unlink,|,pastephp,|,forecolor,'
-                                      + 'backcolor,|,smileys,flags,',
+                                      + 'backcolor,blockquote,|,smileys,flags,',
     theme_advanced_buttons2           : 'paste,pastetext,pasteword,|,search,replace,|,image,|,tablecontrols,|,dzcpuser,|,media',
     theme_advanced_buttons3           : 'fontselect,fontsizeselect,|,insertdate,inserttime,|,sub,sup,|,outdent,indent,|,fullscreen,clip,spellchecker,code,youtube',
     extended_valid_elements           : 'img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],'
