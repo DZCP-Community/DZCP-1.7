051d70b6bb82d04b7c1c6c8338b71d34efdd0f9a
Lucas <godkiller_nt@hammermaps.de>
2015-05-24 03:04:18 +0200

    Use fileExists()

--- inc/additional-functions/dzcp_steamproxy.php
+++ inc/additional-functions/dzcp_steamproxy.php
@@ -20,15 +20,13 @@ function SteamAPI_Proxy($steamID='',$call='',$data0=array()) {
     if(!allow_url_fopen_support()) return false;
     if(is_array($data0)) $send = array_merge(array('steamid' => $steamID,'call' => $call), $data0);
     else $send = array_merge(array('steamid' => $steamID,'call' => $call));
-    $ctx = stream_context_create(array('http'=>array('timeout' => file_get_contents_timeout)));
     $send = bin2hex(gzcompress(base64_encode(serialize($send))));
     $proxy_servers = shuffle_assoc($proxy_servers);
     foreach ($proxy_servers as $server) {
         if(ping_port($server['host'],$server['port'],0.5)) break;
     }
 
-    $ctx = stream_context_create(array('http'=>array('timeout' => file_get_contents_timeout)));
-    if(!($stream = file_get_contents('http://'.$server['host'].':'.$server['port'].'/api.php?proxy=1&data='.$send, false, $ctx))) return false;
+    if(!($stream = fileExists('http://'.$server['host'].':'.$server['port'].'/api.php?proxy=1&data='.$send))) return false;
     if(empty($stream)) return false;
     $ret = unserialize(base64_decode(gzuncompress(hextobin($stream))));
     return array('status' => $ret['status'],'data' => $ret['data']);
